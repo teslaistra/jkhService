a3e20ba3efd09e8b673db28e5a12128fc4fab5e5
Daniil Yefimov <m1802288@edu.misis.ru>
2021-04-03 19:28:38 +0300

    Добавьте файлы проекта.

---
+++ Presentation/Controllers/jkhController.cs
@@ -0,0 +1,23 @@
+﻿using jkhService.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jkhService.Presentation.Controllers
+{   [ApiController]
+    [Route("topics")]
+    public class jkhController: ControllerBase 
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new TopicsModel[]
+            {
+                new TopicsModel(){Name = "Лифтовое оборудование", Description= "лифты и все все все"},
+                new TopicsModel(){Name = "Пожарное оборудование", Description = "вентеляция и прочее"}
+            });
+        }
+    }
+}
---
+++ Presentation/Models/TopicsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jkhService.Presentation.Models
+{
+    public class TopicsModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
