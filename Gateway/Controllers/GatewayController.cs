40842e0c0acc4ee3314fe35cbb2bdc7a90fb2a5a
Daniil Yefimov <m1802288@edu.misis.ru>
2021-05-14 13:56:09 +0300

    cleaned, Gateway done

---
+++ Gateway/Controllers/GatewayController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Sentry;
+using Serilog;
+
+namespace Gateway.Controllers
+{
+    [Route("api/v1/TopicsList")]
+    [ApiController]
+    public class GatewayController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public GatewayController(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        // GET: api/Gateway
+        [HttpGet]
+        public async Task<IActionResult> GetFitnessRecords()
+        {
+            Console.WriteLine("tut");
+            var logger = new LoggerConfiguration()
+                   .WriteTo.Sentry("https://8472251de833404e9ecd48cdfeb6ed00@o661932.ingest.sentry.io/5764923")
+                   .Enrich.FromLogContext()
+                   .CreateLogger();
+
+            var result = "OK";
+            return Ok(result);
+        }
+    }
+}
