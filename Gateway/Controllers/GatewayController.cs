9d6ba4d4a0efdbfbacde8bccdd49e34e8f2a5e83
Daniil Yefimov <m1802288@edu.misis.ru>
2021-06-14 15:57:58 +0300

    made gateway for topics service

--- Gateway/Controllers/GatewayController.cs
+++ Gateway/Controllers/GatewayController.cs
@@ -6,47 +6,113 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Sentry;
 using Serilog;
+using Serilog.Core;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices;
+using System.Text;
+using Gateway.Models;
 
 namespace Gateway.Controllers
 {
-    [Route("api/v1/TopicsList")]
+   
     [ApiController]
     public class GatewayController : ControllerBase
     {
         private readonly IConfiguration _configuration;
 
+        private readonly Logger logger = new LoggerConfiguration()
+                   .WriteTo.Sentry("https://8472251de833404e9ecd48cdfeb6ed00@o661932.ingest.sentry.io/5764923")
+                   .Enrich.FromLogContext()
+                   .CreateLogger();
         public GatewayController(IConfiguration configuration)
         {
-            Console.WriteLine($"gateway555");
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
         // GET: api/Gateway
+        [Route("api/v1/TopicsGet")]
         [HttpGet]
         public async Task<IActionResult> GetTopics()
         {
-            Console.WriteLine($"gateway0");
-            var logger = new LoggerConfiguration()
-                   .WriteTo.Sentry("https://8472251de833404e9ecd48cdfeb6ed00@o661932.ingest.sentry.io/5764923")
-                   .Enrich.FromLogContext()
-                   .CreateLogger();
-            Console.WriteLine($"gateway1");
             try
             {
-
                 logger.Error("Новый поиск тем");
-
                 using HttpClient client = new HttpClient();
                 client.DefaultRequestHeaders.Add("sentry-header", "123");
-                //Реализация обращения к сервису
                 var url = _configuration.GetSection("TopicsURI").Value;
-                Console.WriteLine($"gateway");
+                var resultMessage = await client.GetAsync($"{url}gettopics");
+                resultMessage.EnsureSuccessStatusCode();
+                var result = await resultMessage.Content.ReadAsStringAsync();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e, "Произошла фатальная ошибка");
+                return StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
 
-                var resultMessage = await client.GetAsync($"{url}topics");
-                Console.WriteLine($"{url}topics");
+        [Route("api/v1/EditTopics")]
+        [HttpPost]
+        public async Task<IActionResult> EditTopics([FromBody] object model)
+        {
+            Console.WriteLine("EditTopics");
+            try
+            {
+                Console.WriteLine("EditTopics1");
+                logger.Error("Редактирование темы");
+                using HttpClient client = new HttpClient();
+                var uri = _configuration.GetSection("TopicsURI").Value;
+                var resultMessage = await client.PostAsJsonAsync($"{uri}edittopics", model);
+                var result = await resultMessage.Content.ReadAsStringAsync();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EditTopics2");
+                logger.Fatal(e, "Произошла фатальная ошибка");
+                return StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
 
+        [Route("api/v1/DeleteTopics/{id}")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTopics([FromRoute] string id)
+        {
+            try
+            {   
+                logger.Error("Удаление темы");
+                using (HttpClient client = new HttpClient())
+                {
+                    var uri = _configuration.GetSection("TopicsURI").Value;
+                    var resultMessage = await client.DeleteAsync($"{uri}deletetopics/{id}");
+                    Console.WriteLine($"{uri}deletetopics/{id}");
+                    Console.WriteLine(id);
+                    var result = await resultMessage.Content.ReadAsStringAsync();
+                    return Ok(result);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e, "Произошла фатальная ошибка");
+                return StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
+
+        [Route("api/v1/AddTopics")]
+        [HttpPost]
+        public async Task<IActionResult> AddTopics([FromBody] object model)
+        {
+            try
+            {
+                logger.Error("Добавление темы");
+                using HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("sentry-header", "123");
+                var url = _configuration.GetSection("TopicsURI").Value;
+                var resultMessage = await client.PostAsJsonAsync($"{url}AddTopics", model);
                 resultMessage.EnsureSuccessStatusCode();
                 var result = await resultMessage.Content.ReadAsStringAsync();
                 return Ok(result);
---
+++ Gateway/Models/TopicModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gateway.Models
+{
+    public class TopicModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Id { set; get; }
+        public TopicModel()
+        {
+
+        }
+    }
+}
